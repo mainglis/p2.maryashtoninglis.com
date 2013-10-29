3d7a4c674b82c899812ff73232f48fd637050397
Mary Ashton Inglis <maryashton.inglis@gmail.com>
2013-10-28 20:36:00 -0400

    Edited index.php

--- index.php
+++ index.php
@@ -2,48 +2,41 @@
 
 # The DOC_ROOT and APP_PATH constant have to happen in the actual app
 
-	# Document root, ex: /path/to/home/app.com/../ (uses ./ on CLI)
-	define('DOC_ROOT', empty($_SERVER['DOCUMENT_ROOT']) ? './' : realpath($_SERVER['DOCUMENT_ROOT']).'/../');
-	  
-	# App path, ex: /path/to/home/app.com/
-	define('APP_PATH', realpath(dirname(__FILE__)).'/');
+        # Document root, ex: /path/to/home/app.com/../ (uses ./ on CLI)
+        define('DOC_ROOT', empty($_SERVER['DOCUMENT_ROOT']) ? './' : realpath($_SERVER['DOCUMENT_ROOT']).'/../');
+          
+        # App path, ex: /path/to/home/app.com/
+        define('APP_PATH', realpath(dirname(__FILE__)).'/');
          
 # Environment
-	require_once DOC_ROOT.'environment.php'; 
+        require_once DOC_ROOT.'environment.php'; 
    
 # Where is core located?
-	define('CORE_PATH',  $_SERVER['DOCUMENT_ROOT']."/../core/");
-	   
+        define('CORE_PATH',  $_SERVER['DOCUMENT_ROOT']."/../core/");
+           
 # Load app configs
-	require APP_PATH."/config/config.php";
-	require APP_PATH."/config/feature_flags.php";
-	  
+        require APP_PATH."/config/config.php";
+        require APP_PATH."/config/feature_flags.php";
+          
 # Bootstrap
-	require CORE_PATH."bootstrap.php";
+        require CORE_PATH."bootstrap.php";
 
 # Routing
     Router::$routes = array(
-    	'/' => '/index',     # default controller when "/" is requested
+            '/' => '/index',     # default controller when "/" is requested
     );
     
 # Match requested uri to any routes and instantiate controller
     Router::init();
     
 # Display environment details
-	require CORE_PATH."environment-details.php";
-	
+        require CORE_PATH."environment-details.php";
+        
+        
+        
+        
+        
+        
+        
+        
 ?>
-<?php if($new_user): ?>
-
-    <h2>Welcome New User!</h2>
-    <form>
-        <label for='first_name'>First Name<br>
-        <input type='text' name='first_name' id='first_name'>
-
-        <label for='email'>Email<br>
-        <input type='text' name='email' id='email'>
-
-        <input type='submit'>
-    </form>
-
-<?php endif; ?>
